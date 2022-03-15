a91a336422c94c48b44341bbd504981fe5562db9
ChungHueiTsao <chunghuei.tsao@gmail.com>
2022-03-15 09:46:24 +0800

    huei

--- index.js
+++ index.js
@@ -1,48 +1,50 @@
 // localStorage.removeItem("todoList");
 // console.log(JSON.parse(localStorage.getItem("todoList")).todo);
 
-if (JSON.parse(localStorage.getItem("todoList")) == null) {} else {
-  for (
-    var i = 0; i < JSON.parse(localStorage.getItem("todoList")).title.length; i++
-  ) {
-    $("#list").append(`
-              <div class="article">
-                  <h2>${
-                    JSON.parse(localStorage.getItem("todoList")).title[i]
-                  }</h2>
-                  <p>${
-                    JSON.parse(localStorage.getItem("todoList")).content[i]
-                  }</p>
-              </div>
-          `);
-  }
-}
+// if (JSON.parse(localStorage.getItem("todoList")) !== null) {
+
+//   for (
+//     var i = 0; i < JSON.parse(localStorage.getItem("todoList")).title.length; i++
+//   ) {
+//     $("#list").append(`
+//               <div class="article">
+//                   <h2>${
+//                     JSON.parse(localStorage.getItem("todoList")).title[i]
+//                   }</h2>
+//                   <p>${
+//                     JSON.parse(localStorage.getItem("todoList")).content[i]
+//                   }</p>
+//               </div>
+//           `);
+//   }
+// }
 
 $("#btn").on("click", function () {
   var title = $("#title").val();
   var content = $("#content").val();
-  console.log("tit");
 
   const data = localStorage.getItem("todoList") ?
     JSON.parse(localStorage.getItem("todoList")) : {
       contentArray: [],
       titleArray: [],
     };
+  console.log(title);
   data.titleArray.push(title);
   data.contentArray.push(content);
   localStorage.setItem("todoList", JSON.stringify(data));
 
+  console.log("標題:", title)
   $("#list").append(`
-        <div class="article">
-            <h2>${title}</h2>
-            <p>${content}</p>
-        </div>
+    <div class="article">
+    <h2>${title}</h2>
+    <p>${content}</p>
+    </div>
     `);
+  // console.log(data);
 
   $("#title").val("");
   $("#content").val("");
 
-  // console.log("標題:",tit)
   // console.log("內容:",content)
   // console.log("文章:",list)
 });
